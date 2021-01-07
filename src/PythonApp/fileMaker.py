7c729b85d53f6f5ec27d8663bd7c838f6668003a
jdv52 <66391744+jdv52@users.noreply.github.com>
2021-01-06 19:24:36 -0500

    Update fileMaker.py

--- src/PythonApp/fileMaker.py
+++ src/PythonApp/fileMaker.py
@@ -1,12 +1,18 @@
 import csv
+import random
 
 def main():
 	with open('names.csv', 'w', newline="") as csvfile:
-		fieldnames = ['first_name', 'last_name']
+		fieldnames = ['x', 'y', 'z', 'gy-x', 'gy-y', 'gy-z']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
 		writer.writeheader()
-		writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+		for i in range(100):
+			nums = []
+			for j in range(len(fieldnames)):
+				random.seed()
+				nums.append(random.randint(0, 100))
+			writer.writerow({'x': nums[0], 'y': nums[1], 'z':nums[2], 'gy-x':nums[3], 'gy-y':nums[4], 'gy-z':nums[5]})
 
 
 main()
